90e2d497c05484bd7ca0c908dbccc33cf4090797
lyf <1445184151@qq.com>
2018-05-17 20:31:48 +0800

    fix deps path

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -7,7 +7,7 @@ set -e
 
 main()
 {
-    cd ../deps
+    cd ./deps
     tar -zxf libopenblas.tar.gz && \
     sudo dpkg -i libblas-common_3.6.0-2ubuntu2_amd64.deb && \
     sudo dpkg -i libopenblas-base_0.2.18-1ubuntu1_amd64.deb && \
